7d29569396e7443535f1362a056ca193247f1a03
Piia Ruokonen <piiar>
2019-01-25 21:34:54 +0200

    Pick up and drop

--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -2,41 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementManager : MonoBehaviour
-{
+public class MovementManager : MonoBehaviour {
     Vector3 movement;
-    bool interaction;
 
     float rotationSpeed = 12f;
     float moveSpeed = 6f;
 
+    GameObject carriedObject = null;
+
     private new Rigidbody rigidbody;
 
     // Start is called before the first frame update
-    void Awake()
-    {
+    void Awake() {
         rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    public void Move(Vector3 moveDirection, bool interaction)
-    {
+    public void Move(Vector3 moveDirection, bool interaction) {
         ApplyRotationTo(moveDirection);
 
         // Move the controller
         var movement = moveDirection * moveSpeed * Time.deltaTime;
 
         rigidbody.MovePosition(transform.position + movement);
+
+        HandleInteraction(interaction);
     }
 
-    private void ApplyRotationTo(Vector3 targetPosition)
-    {
+    private void ApplyRotationTo(Vector3 targetPosition) {
         Vector3 repositioning = targetPosition;
-        if (repositioning != Vector3.zero)
-        {
+        if (repositioning != Vector3.zero) {
             repositioning.y = 0;
             Quaternion targetRotation = Quaternion.LookRotation(repositioning, Vector3.up);
             rigidbody.MoveRotation(Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime));
+        }
+    }
+
+    private GameObject FindNearestObject(Vector3 center) {
+        float radius = 2f;
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        if (hitColliders.Length > 1) { // 0 is Player
+            return hitColliders[1].gameObject;
+        }
+        return null;
+    }
+
+    private void HandleInteraction(bool interaction) {
+        if (interaction) {
+            if (carriedObject != null) {
+                // Drop
+                carriedObject.transform.SetParent(null);
+                carriedObject = null;
+            }
+            else {
+                // Pick up
+                GameObject obj = FindNearestObject(transform.position);
+                if (obj) {
+                    obj.transform.SetParent(transform);
+                    carriedObject = obj;
+                }
+            }
         }
     }
 }
