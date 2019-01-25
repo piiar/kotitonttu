57b87bc33f6303409fa6ad1236842f3372ce757a
Markku Painomaa <markku.painomaa@visma.com>
2019-01-25 19:52:25 +0200

    character ctrl

---
+++ Assets/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private MovementManager movementManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        movementManager = GetComponent<MovementManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // read inputs
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool interaction = Input.GetKeyDown(KeyCode.Space);
+        Vector3 moveDirection = v * Vector3.forward + h * Vector3.right;
+
+        movementManager.Move(moveDirection, interaction);
+    }
+}
+
+// public class InputData
+// {
+
+//     public Vector3 move;
+//     public Vector3 forward;
+//     public Vector2 axis;
+//     public bool interaction;
+
+
+//     public InputData(Vector3 move, Vector3 forward, Vector2 axis, bool interaction = false)
+//     {
+//         this.move = move;
+//         this.forward = forward;
+//         this.axis = axis;
+//         this.interaction = interaction;
+//     }
+// }
---
+++ Assets/MovementManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    Vector3 movement;
+    bool interaction;
+
+    float rotationSpeed = 12f;
+    float moveSpeed = 6f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Move(Vector3 moveDirection, bool interaction)
+    {
+        ApplyRotationTo(moveDirection);
+
+        // Move the controller
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void ApplyRotationTo(Vector3 targetPosition)
+    {
+        Vector3 repositioning = targetPosition;
+        if (repositioning != Vector3.zero)
+        {
+            repositioning.y = 0;
+            Quaternion targetRotation = Quaternion.LookRotation(repositioning, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
