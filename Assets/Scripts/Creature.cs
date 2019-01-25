f238bb8c75c9fb217f0c946a91fe1c4a24459b34
Piia Ruokonen <piiar>
2019-01-25 22:54:44 +0200

    Creature follows goals that were moved

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Creature : MonoBehaviour
-{
+public class Creature : MonoBehaviour {
     public Transform goal;
+    private NavMeshAgent agent;
 
-    void Awake()
-    {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (goal.parent == null) { // target is not being carried
+            agent.destination = goal.position;
+        }
     }
 }
