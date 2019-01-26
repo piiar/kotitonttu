ba1a297ff46abe670c0180bc80c9841fa3e0f5ec
Vazde <vazde with a domain of dea dot fi>
2019-01-26 18:41:15 +0200

    Score stuff

---
+++ Assets/Scripts/ScoreGuiScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScoreGuiScript : MonoBehaviour {
+    public GUIStyle Style;
+
+    [Range(0, 1000)]
+    public int X;
+    [Range(0, 1000)]
+    public int Y;
+
+    [Range(-50, 50)]
+    public int Spacing;
+
+    void OnGUI() {
+         Vector2 pos = new Vector2(X, Y);
+         pos = drawText(pos, "Score: " + (long)ScoreScript.Score);
+         pos = drawText(pos, "Score max: " + (long)ScoreScript.ScoreMax);
+         pos = drawText(pos, "Delta: " + (long)(ScoreScript.ScoreMax - ScoreScript.Score));
+         pos = drawText(pos, "Damged objects: " + ScoreScript.NumDamagedGoals);
+    }
+
+    // Draw text and increment Y position
+    private Vector2 drawText(Vector2 pos, string text) {
+        Vector2 size = new Vector2(1000, 300);
+
+        Rect r = new Rect(pos, size);
+        GUI.Label(r, text, Style);
+
+        float height = Style.CalcHeight(new GUIContent(text), size.x);
+
+        return new Vector2(pos.x, pos.y + height + Spacing);
+    }
+}
---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+    public static double Score;
+    public static double ScoreMax;
+    public static int NumDamagedGoals;
+
+    private List<Item> items;
+    
+    // Start is called before the first frame update
+    void Start() {
+        Score = 0;
+        ScoreMax = 0;
+        NumDamagedGoals = 0;
+
+        items = new List<Item>();
+        foreach (var item in GameObject.FindObjectsOfType<Item>()) {
+            if (item.isGoal) {
+                items.Add(item);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        double tickScore = 0;
+        double tickScoreMax = 0;
+        int tickNumDamagedGoals = 0;
+        foreach (var item in items) {
+            double itemScore = item.currentValue / item.maxValue;
+            tickScore += itemScore;
+            tickScoreMax += 1;
+
+            if (item.currentValue < item.maxValue) {
+                tickNumDamagedGoals++;
+            }
+        }
+
+        Score += tickScore * Time.deltaTime;
+        ScoreMax += tickScoreMax * Time.deltaTime;
+        NumDamagedGoals = tickNumDamagedGoals;
+    }
+}
