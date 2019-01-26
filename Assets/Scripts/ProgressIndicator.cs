c2bde47fd455cd8b8d9b885b69faf7282e19551a
Piia Ruokonen <piiar>
2019-01-26 22:56:09 +0200

    Progress indicator script

---
+++ Assets/Scripts/ProgressIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressIndicator : MonoBehaviour {
+    private int max = 100;
+    private int current = 50;
+
+    // Update is called once per frame
+    void Update() {
+        GetComponent<Image>().fillAmount = FillAmount();
+    }
+
+    public void SetMax(int max) {
+        this.max = max;
+    }
+
+    private float FillAmount() {
+        return current / max;
+    }
+}
