dc872c0574265c205260f41e239ddd0bcab0cca9
Ari Rusakko <ari.rusakko@octo3.fi>
2019-01-26 14:24:29 +0200

    few script updates

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -13,6 +13,33 @@ public class Item : MonoBehaviour {
     public ItemType itemType;
     public bool isGoal;
     public bool isCarryable;
+    public bool isFixable;
     public int maxValue;
     public int currentValue;
+    public int damageFactor;
+    public int fixFactor;
+
+    private void HandleDamage()
+    {
+        if(this.currentValue > 0)
+        {
+            this.currentValue -= this.damageFactor;
+            if (this.currentValue < 0)
+            {
+                this.currentValue = 0;
+            }
+        }
+    }
+
+    private void HandleFixing()
+    {
+        if (this.currentValue < this.maxValue)
+        {
+            this.currentValue += this.fixFactor;
+            if (this.currentValue > this.maxValue)
+            {
+                this.currentValue = this.maxValue;
+            }
+        }
+    }
 }
